06e6323d1c6ec401387734823fefb4208a6a61a9
Masoud Khodadadi <masoud.xpress@gmail.com>
2020-06-19 18:51:32 +0430

    Add MailHost Migration

--- MailMicroService/Migrations/20200619141947_AddMailHost.Designer.cs
+++ MailMicroService/Migrations/20200619141947_AddMailHost.Designer.cs
@@ -3,14 +3,16 @@ using MailMicroService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MailMicroService.Migrations
 {
     [DbContext(typeof(MailMicroServiceContext))]
-    partial class MailMicroServiceContextModelSnapshot : ModelSnapshot
+    [Migration("20200619141947_AddMailHost")]
+    partial class AddMailHost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MailMicroService/Migrations/20200619141947_AddMailHost.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MailMicroService.Migrations
+{
+    public partial class AddMailHost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MailServer",
+                columns: new[] { "Id", "Email", "Host", "Password", "Port", "ServerType" },
+                values: new object[] { 1, "masoud.xpress@gmail.com", "smtp.gmail.com", "MASOUD7559", 587, "gmail" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MailServer",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
